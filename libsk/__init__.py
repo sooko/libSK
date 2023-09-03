99874719117de9880c3c3df16087566615547bee
sooko <hansmotor89@gmail.com>
2023-09-04 00:04:22 +0700

    staet

---
+++ libsk/__init__.py
@@ -0,0 +1,1 @@
+from .uix.navigationdrawer import NavigationDrawer
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='libsk',
-    version='1.0',
+    version='1.1',
     packages=find_packages(),
     url='https://github.com/sooko/libsk.git',
     license='MIT',
