467fc0cf441ac7c57368fcf13b01ddfad7739add
sooko <hansmotor89@gmail.com>
2023-09-03 23:58:12 +0700

    fist

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+    name='libsk',
+    version='1.0',
+    packages=find_packages(),
+    url='https://github.com/sooko/libSK.git',
+    license='MIT',
+    author='Burhan',
+    author_email='hansmotor89@gmail.com',
+    description='Description of your project',
+    install_requires=[
+        'kivy',
+    ],
+)
